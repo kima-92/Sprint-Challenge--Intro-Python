20039bd70f59032bc9b0cf66f057778979e7bf2e
Wilma <kima_92@fastmail.com>
2020-04-10 12:21:37 -0400

    Used the CSV Module to itirate through the cities in 'cities.csv' and create a City object for each one

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -10,8 +10,6 @@ class City:
     self.lat = lat
     self.lon = lon
 
-
-
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -25,20 +23,37 @@ class City:
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 
-  with open("cities.csv", newline="") as csvfile:
-    cities_reader = csv.reader(csvfile, delimiter=" ", quotechar="|")
-    
-    for row in cities_reader:
-
-      print("| |".join(row))
-      
 cities = []
 
+# This function, use the CSV module to read 'cities.csv
+# Each record should be imported into an instance
+# Return the list with all the cities instances
+
+# Note: the first line of the CSV is a header that describes the fields--this
+# should not be loaded into a City object.
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+
+    with open("cities.csv", newline="") as csvfile:
+      cities_reader = csv.reader(csvfile, delimiter=",")
+
+      count = 1
+      
+      for row in cities_reader:
+
+        if count != 1:
+          name = row[0]
+          lat = float(row[3])
+          lon = float(row[4])
+
+          city = City(name, lat, lon)
+          print(f"\ncreated object for: {city.name}\n")
+          #print("| |".join(row))
+
+        count += 1
+
     return cities
 
 cityreader(cities)
